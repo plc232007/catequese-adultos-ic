653b690e6ee1ed966a0155d27e810ee74e8f5678
Pedro Leite Campos <pleitecampos@gmail.com>
2026-09-09 16:24:48 -0300

    chore: atualiza cache do aplicativo para v12

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estratégia: Cache-first com atualização em background (SWR)
    ================================================================ */
 
-const CACHE = 'ic-2026-v11';
+const CACHE = 'ic-2026-v12';
 
 const PRECACHE = [
   '/',
